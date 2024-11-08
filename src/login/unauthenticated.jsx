09ba00f4c99badd4eb9c828d30447f95b8b91458
Maximiliano Paris <mparis@hotmail.cl>
2024-11-07 21:06:49 -0700

    feat: handleAuthentication() with testUser

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -3,22 +3,43 @@ import {AuthState} from "./authState";
 
 export default function Unauthenticated({ onAuthenticate }) {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
+    const [usernameLabel, setUsernameLabel] = useState("");
+    const [passwordLabel, setPasswordLabel] = useState("")
+    const [error, setError] = useState("");
+    let authenticatedUser = {}
+
+    const testUser = {
+        username: "a",
+        password: "a",
+        firstName: "Jack",
+        lastName: "Harrison",
+    }
+
+    function handleAuthentication() {
+        if (authenticateUser()) {
+            authenticatedUser = testUser;
+            onAuthenticate(authenticatedUser.firstName, AuthState.Authenticated)
+        } else {
+            setError("The password you have entered does not exist.")
+        }
+    }
+
+    function authenticateUser() {
+        return usernameLabel === testUser.username && passwordLabel === testUser.password
+    }
 
     return (
         <>
             <h1>Log in to your account</h1>
             <form className="login-form-width d-flex flex-column align-items-center justify-content-center">
                 <div className="form-group my-3 w-100">
-                    <label htmlFor="inputEmail1">Email address</label>
+                    <label htmlFor="inputUsername1">Username</label>
                     <input
-                        type="email"
+                        type="text"
                         className="form-control w-100"
-                        id="inputEmail1"
-                        aria-describedby="emailHelp"
-                        placeholder="Enter email"
-                        onChange={(e) => setEmail(e.target.value)}
+                        id="inputUsername1"
+                        placeholder="Enter username"
+                        onChange={(e) => setUsernameLabel(e.target.value)}
                     />
                 </div>
 
@@ -29,17 +50,28 @@ export default function Unauthenticated({ onAuthenticate }) {
                         className="form-control w-100"
                         id="inputPassword1"
                         placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={(e) => setPasswordLabel(e.target.value)}
                     />
                 </div>
             </form>
+
+
+
             <button
                 type="submit"
                 className="login-form-width btn btn-primary"
-                onClick={ () => onAuthenticate(email, AuthState.Authenticated) }
+                disabled={ usernameLabel === "" || passwordLabel === "" }
+                onClick={ () => handleAuthentication() }
+                // onClick={ () => setError("The passwordLabel you have entered does not exist") }
+                // onClick={ () => onAuthenticate(usernameLabel, AuthState.Authenticated) }
+
             >
                 Login
             </button>
+
+            { error &&
+                <h5 className="text-danger m-3 ">{error}</h5>
+            }
         </>
     );
 }
