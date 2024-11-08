8f6f5b406ce1d8ddfcf70c69e49c7a346dad95d9
Maximiliano Paris <mparis@hotmail.cl>
2024-11-07 20:32:05 -0700

    fix: buttons in signUpInvitationCard component

--- src/home/signUpInvitationCard.jsx
+++ src/home/signUpInvitationCard.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import {Button} from "react-bootstrap";
 
 export default function SignUpInvitationCard() {
+    const navigateTo = useNavigate();
     return (
         <div className="custom-card card text-center p-4 my-5">
             <div className="card-body">
                 <div className="my-3">
                     <h4>Try it today! 👇</h4>
-                    <a href="../signup/signup.html" className="btn btn-primary btn-lg mb-3">Create account</a>
+                    <Button
+                        size="lg"
+                        onClick={ () => navigateTo("/signup")}>
+                        Create Account
+                    </Button>
                 </div>
 
                 <div className="my-">
                     <h4>Already a member? ✅</h4>
-                    <a href="../login/login.html" className="btn btn-outline-primary btn-lg">Log in to your
-                        account</a>
+                    <Button
+                        size="lg"
+                        onClick={ () => navigateTo('/login') }
+                    >
+                        Log in to your account
+                    </Button>
                 </div>
             </div>
         </div>
